680c33f4e75a730758d48e9d34267e65e48c312f
hiddenntreasure <mdkito@yahoo.com>
2020-05-23 19:08:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ import tensorflow as tf
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'res.h5'
+#MODEL_PATH = 'res.h5'
 
 #config = tf.ConfigProto(
 #        device_count={'CPU': 1},
@@ -37,15 +37,15 @@ MODEL_PATH = 'res.h5'
 #keras.backend.set_session(session)
 
 # Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
+#model = load_model(MODEL_PATH)
+#model._make_predict_function()          # Necessary
 # print('Model loaded. Start serving...')
 
 # You can also use pretrained model from Keras
 # Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
+from keras.applications.resnet50 import ResNet50
+model = ResNet50(weights='imagenet')
+model.save('')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
